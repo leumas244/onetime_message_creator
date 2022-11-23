2ccb6b950ac6b6c691e3d2c459bc0d945ec9c137
leumas244 <FreddiW.15>
2022-11-23 12:42:59 +0100

    add additional User info

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import AdditionalUserInfo
 
 # Register your models here.
+class AdminAdditionalUserInfo(admin.ModelAdmin):
+    list_display = ['id',
+                    'user',
+                    'gender',
+                    'update_date',
+                    'creation_date',
+                    ]
+
+admin.site.register(AdditionalUserInfo, AdminAdditionalUserInfo)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class AdditionalUserInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    gender = models.CharField('Geschlecht', blank=False, null=False, max_length=20)
+
+    update_date = models.DateTimeField('Geändert', auto_now=True)
+    creation_date = models.DateTimeField('Erstellt', auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "AdditionalUserInfos"
+
+    def __str__(self):
+        return self.user.username
